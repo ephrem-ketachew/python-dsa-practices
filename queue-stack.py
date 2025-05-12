a2c44f6484b166d0951e170fb1cb864435a60fb6
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-05-12 08:40:40 +0300

    Implemented enqueue and dequeue using 2 stacks

---
+++ queue-stack.py
@@ -0,0 +1,20 @@
+class MyQueue:
+    def __init__(self, value):
+        self.stack1 = []
+        self.stack2 = []
+        
+    def print(self):
+        for i in range(len(self.stack1)):
+            print(self.stack1.value)
+            
+    def enqueue(self, value):
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        
+        self.stack1.append(value)
+        
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+            
+    def dequeue(self):
+        return self.stack1.pop() if self.stack1 else None
