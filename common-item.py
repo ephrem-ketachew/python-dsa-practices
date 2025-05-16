eb7ce73d3458c9bcfae7c8af9e3c01cac2dd2138
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-05-16 10:46:02 +0300

    checked whether two lists have common item using dict with O(n) time complexity

---
+++ common-item.py
@@ -0,0 +1,17 @@
+def item_in_common(list1, list2):
+    my_dict = {}
+    for item in list1:
+        my_dict[item] = True
+    
+    return any(item in my_dict for item in list2)
+    
+    
+    
+    # return any(item1 == item2 for item1 in list1 for item2 in list2)
+    
+    
+    # for item1 in list1:
+    #     for item2 in list2:
+    #         if item1 == item2:
+    #             return True
+    # return False
