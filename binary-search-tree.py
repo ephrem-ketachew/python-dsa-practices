a596b11f2fcfda516517d69bb74818fd21e05776
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-05-26 10:49:59 +0300

    Recursive implementation of contains and insert

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -60,3 +60,46 @@ class BinarySearchTree:
                 return True
             
         return False
+    
+    def __r_contains(self, node, value):
+        if node == None:
+            return False
+        if node.value == value:
+            return True
+        if value < node.value:
+            return self.__r_contains(node.left, value)
+        if value > node.value:
+            return self.__r_contains(node.right, value)
+    
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
+    
+    def __r_insert(self, node, value):
+        # if node is None:
+        #     return Node(value)
+        # if value < node.value:
+        #     node.left = self.__r_insert(node.left, value)
+        # if value > node.value:
+        #     node.right = self.__r_insert(node.right, value)
+        # return node
+        if node.value == value:
+            return False
+        if value < node.value:
+            if node.left is None:
+                new_node = Node(value)
+                node.left = new_node
+                return True
+            return self.__r_insert(node.left, value)
+        if value > node.value:
+            if node.right is None:
+                new_node = Node(value)
+                node.right = new_node
+                return True
+            return self.__r_insert(node.right, value)
+    
+    def r_insert(self, value):
+        # self.root = self.__r_insert(self.root, value)
+        if self.root is None:
+            self.root = Node(value)
+            return True
+        return self.__r_insert(self.root, value)
