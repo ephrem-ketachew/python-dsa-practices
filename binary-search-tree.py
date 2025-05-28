4a5d99dddc52df3a451ca51ae251d32aebf5363f
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2025-05-28 10:45:35 +0300

    Deleting a node from bst

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -103,3 +103,33 @@ class BinarySearchTree:
             self.root = Node(value)
             return True
         return self.__r_insert(self.root, value)
+    
+    def __delete_node(self, current_node, value):
+        def min_value(self, current_node):
+            while current_node.left is not None:
+                current_node = current_node.left
+            return current_node
+        
+        if current_node == None:
+            return None
+        if value < current_node.value:
+            current_node.left = self.__delete_node(current_node, value)
+        elif value > current_node.value:
+            current_node.right = self.__delete_node(current_node, value)
+        else:
+            if current_node.left == None and current_node.right == None:
+                return None
+            elif current_node.left == None:
+                current_node = current_node.right
+            elif current_node.right == None:
+                current_node = current_node.left
+            else:
+                subtree_min = min_value(current_node.right)
+                current_node.value = subtree_min
+                current_node.right = self.__delete_node(current_node.right, subtree_min)
+                
+        return current_node
+    
+    def delete_node(self, value):
+        self.__delete_node(self.root, value)
+    
